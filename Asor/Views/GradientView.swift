526dc46f832d9b8a5edd5d5b183aef87d1a36f78
tanaykumar018 <tanaykumar018@gmail.com>
2017-09-29 19:17:31 -0400

    Update Channel ViewController UI.

---
+++ Asor/Views/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Asor
+//
+//  Created by Tanay Kumar on 9/29/17.
+//  Copyright © 2017 Tanay Kumar. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor : UIColor = #colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor : UIColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
