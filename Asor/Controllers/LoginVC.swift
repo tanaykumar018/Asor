43b9d33816e04a18dc4071d5ee6caddf4f80d1dc
tanaykumar018 <tanaykumar018@gmail.com>
2017-09-29 21:38:53 -0400

    Add Login View.

---
+++ Asor/Controllers/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Asor
+//
+//  Created by Tanay Kumar on 9/29/17.
+//  Copyright © 2017 Tanay Kumar. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Asor/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Asor
+//
+//  Created by Tanay Kumar on 9/29/17.
+//  Copyright © 2017 Tanay Kumar. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
