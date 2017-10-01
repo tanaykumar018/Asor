e86f76593e5689fef00e969ed97cabcd8161a110
tanaykumar018 <tanaykumar018@gmail.com>
2017-10-01 11:01:38 -0400

    Logged in user

--- Asor/Controllers/CreateAccountVC.swift
+++ Asor/Controllers/CreateAccountVC.swift
@@ -37,7 +37,11 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: pass) { (success) in
             if success {
-                print("register user!")
+                AuthService.instance.loginUser(email: email, password: pass, completion: { (success) in
+                    if success {
+                        print("logged in user!", AuthService.instance.authToken)
+                    }
+                })
             }
         }
     }
