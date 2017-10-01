975f64abbb15c9444a26a047af6ef70d0d117b1e
tanaykumar018 <tanaykumar018@gmail.com>
2017-09-30 23:27:56 -0400

    Add AuthService

--- Asor/Utilities/Constants.swift
+++ Asor/Utilities/Constants.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+typealias  complitionHandler = (_ Success: Bool) -> ()
+
+// URL Constant
+let BASE_URL = "https://asoradda.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// Segue
 let TO_LOGIN = "toLogin"
 let TO_Create_Account = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
