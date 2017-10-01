18e79f00fbdecc6fbf8dbabc046a0636ed2a5cd3
tanaykumar018 <tanaykumar018@gmail.com>
2017-09-30 23:30:48 -0400

    Add AuthService ErrorSolved

--- Asor/Services/AuthService.swift
+++ Asor/Services/AuthService.swift
@@ -58,7 +58,7 @@ class AuthService {
         Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
             
             if response.result.error == nil {
-                complition(true)
+                completion(true)
             } else {
                 completion(false)
                 debugPrint(response.result.error as Any)
